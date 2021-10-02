b9150e1233ea020c2c450ca2edbaa0cacaccd17f
Alfredo Braule <alfredoneto934@gmail.com>
2021-10-02 15:40:21 -0300

    method index on AgendamentosController and method findAll on AgendamentosRepository created

--- src/Controller/AgendamentosController.php
+++ src/Controller/AgendamentosController.php
@@ -3,6 +3,7 @@
 namespace AgVeiculo\Controller;
 
 use AgVeiculo\Entity\Agendamento;
+use AgVeiculo\Lib\AuthorizationException;
 use AgVeiculo\Lib\JsonResponse;
 use AgVeiculo\Repository\AgendamentosRepository;
 use AgVeiculo\Lib\BadRequestException;
@@ -20,7 +21,18 @@ class AgendamentosController
 
     public function index($request)
     {
-        
+        try {
+            $veiculosAgendados = $this->agendamentosRepository
+                ->findAll(property_exists($request, "veiculoId") ? $request->veiculoId : null);
+            return new JsonResponse(['veiculos' => $veiculosAgendados], 200);
+        } catch (PDOException $e) {
+            file_put_contents('log.txt', $e->getMessage() . '\n', FILE_APPEND);
+            return new JsonResponse(['mensagem' => 'Ocorreu um erro no banco de dados! Favor tente novamente!'], 500);
+        } catch (AuthorizationException $e) {
+            return new JsonResponse(['mensagem' => $e->getMessage()], 401);
+        } catch (Exception $e) {
+            return new JsonResponse(['mensagem' => $e->getMessage()], 500);
+        }
     }
     
     public function create($request)
--- src/Repository/AgendamentosRepository.php
+++ src/Repository/AgendamentosRepository.php
@@ -6,6 +6,27 @@ use PDO;
 
 class AgendamentosRepository
 {
+
+    public function findAll($veiculoId = null)
+    {
+        $filter = '';
+        if($veiculoId != null) {
+            $filter = "where id = :veiculoId";
+        }
+
+        $pdo = DbConnectionFactory::get();
+        $sql = "SELECT * FROM Agendamento $filter";
+        $statement = $pdo->prepare($sql);
+
+        if ($veiculoId != null) {
+            $statement->bindValue(":veiculoId", $veiculoId);
+        }
+        
+        $statement->execute();
+        $veiculosAgendados = $statement->fetchAll(PDO::FETCH_OBJ);
+        return $veiculosAgendados;
+    }
+
     public function create($agendamento)
     {
         var_dump($agendamento);
